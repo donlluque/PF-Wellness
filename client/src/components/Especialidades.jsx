7bb07000581b506e203f7b8caf8cd2a1eec4eb58
ValentinaBonfanti <bonfantivalentina30@gmail.com>
2022-08-16 16:47:04 -0300

    feature/front/components/especialidades

--- client/src/components/Especialidades.jsx
+++ client/src/components/Especialidades.jsx
@@ -1,5 +1,16 @@
 function Especialidades() {
-  return <div>Especialidades</div>;
+  return <div>
+    <div>Especialidades</div>
+    <div>
+      <h3>Traumatologia</h3>
+      <p>texto breve de descripcion</p>
+      <ul>
+        <li>subespecialidades</li>
+      </ul>
+      <img/>
+    </div>
+    
+  </div>;
 }
 
 export default Especialidades;

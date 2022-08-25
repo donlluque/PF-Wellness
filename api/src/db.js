15037742bb2ad91dac3d052689b1e29c8cb222fa
AgusCue <cuelloagustin3@gmail.com>
2022-08-25 10:00:52 -0300

    fix/back/db(nom=db)&indexDeFront

--- api/src/db.js
+++ api/src/db.js
@@ -30,7 +30,7 @@ let sequelize =
         ssl: true,
       })
     : new Sequelize(
-        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/wellness`,
+        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
 
         { logging: false, native: false }
       );

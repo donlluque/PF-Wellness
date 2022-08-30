65835c756b5c91ea9390dd95c48bc8fe5cee2516
Jhon Bv <jhonba2020@gmail.com>
2022-08-30 11:57:36 -0500

    fix route put doctor, add model absence with associations

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -11,6 +11,7 @@ const work_days = require("./work_days.js");
 const hours_working = require("./hours_working.js");
 const pagos_unicos = require("./mercado_pago.js");
 const pagos_asociados = require("./mercado_asociados.js");
+const absence = require("./absence.js");
 
 const router = Router();
 
@@ -21,6 +22,7 @@ router.use("/patients", patients);
 router.use("/prepaid_health", prepaid_health);
 router.use("/work_days", work_days);
 router.use("/hours_working", hours_working);
+router.use("/absence", absence);
 router.use("/filter", filtro);
 router.use("/dates", dates);
 router.use("/checkuser", checkUser);

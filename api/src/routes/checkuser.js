8ac2955450ad72319734c0ca77cd27604395463d
AgusCue <cuelloagustin3@gmail.com>
2022-09-01 19:09:29 -0300

    feature/back/checkuser

--- api/src/routes/checkuser.js
+++ api/src/routes/checkuser.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const router = Router();
-const { Patient, Prepaid_health } = require("../db.js");
+const { Patient, Prepaid_health, Doctor } = require("../db.js");
 
 router.post("/", async (req, res, next) => {
   // try {
@@ -14,10 +14,45 @@ router.post("/", async (req, res, next) => {
     },
   });
 
+  const doctor = await Doctor.findOne({
+    where: { email: email },
+    include: [
+      {
+        model: Prepaid_health,
+        attributes: ["id", "name"],
+      },
+      {
+        model: Work_days,
+        throught: {
+          attributes: [],
+        },
+      },
+      {
+        model: Absence,
+        throught: {
+          attributes: [],
+        },
+      },
+      {
+        model: General_area,
+        throught: {
+          attributes: [],
+        },
+      },
+      {
+        model: Review,
+        throught: {
+          attributes: [],
+        },
+      },
+    ],
+  });
+
   console.log(patient, "soy patient");
   if (!email) res.status(400).send("Sorry, I need more data to post");
-
-  if (patient) {
+  if (doctor) {
+    return res.status(200).send(doctor);
+  } else if (patient) {
     return res.status(200).send(patient);
   } else if (!given_name) {
     let newPatient = await Patient.create({

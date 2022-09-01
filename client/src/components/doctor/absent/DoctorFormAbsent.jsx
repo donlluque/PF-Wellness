de813a2d5fcb01030dcbfc35bfb8ca5f9864172a
ValentinaBonfanti <bonfantivalentina30@gmail.com>
2022-09-01 10:19:46 -0300

    cambios y pnueva rama

--- client/src/components/doctor/absent/DoctorFormAbsent.jsx
+++ client/src/components/doctor/absent/DoctorFormAbsent.jsx
@@ -10,16 +10,18 @@ model: ausencias
 /* id  / name / discount 
                 0.5*/
 
-import { Radio, RadioGroup, Stack } from "@chakra-ui/react";
+import { Radio, RadioGroup, Stack, Box } from "@chakra-ui/react";
 import { useEffect } from "react";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getHours } from "../../../redux/actions";
-
+import { DatePicker } from "@material-ui/pickers";
 function DoctorFormAbsent() {
   const [form, setForm] = useState({});
   const dispatch = useDispatch();
   const { hoursWorking } = useSelector((state) => state);
+  let aux = [0, 1, 2, 3, 4, 5, 6];
+  const [selectedDate, setDateChange] = useState(""); //cambia fecha en calendario
 
   useEffect(() => {
     dispatch(getHours());
@@ -33,6 +35,25 @@ function DoctorFormAbsent() {
     } else if (e === "extended") {
       setForm({ [e]: { start: "", end: "" } });
     }
+  };
+//funcion que evalua si el dia actual esta deshabilitado --> retorna el dia proxima habilitado
+// const initialDate = (aux) => {
+//   let date = new Date();
+//   for (let i = 0; i < aux.length; i++) {
+//     if (aux[date.getDay()] === null) {
+//       return date;
+//     } else {
+//       date = addDaysToDate(date);
+//     }
+//   }
+// };
+  
+  const handleChangeCalendar = (date) => {
+    //cambio fecha
+    setDateChange(date);
+    setForm({ ...form, date: date.toLocaleDateString() });
+    //validaciones horas disponibles
+    //setArrayTurns(searchTurnsAvailable(hours, totalHours, totalTurns, date));
   };
   return (
     <>
@@ -49,6 +70,23 @@ function DoctorFormAbsent() {
           </Radio>
         </Stack>
       </RadioGroup>
+      {form.extended &&
+          <Box m="1rem" boxShadow={"2xl"} rounded={"md"}>
+                <DatePicker
+                  value={selectedDate}
+                  onChange={(date) => handleChangeCalendar(date)}
+                  disablePast
+                  autoOk
+                  variant="static"
+                  shouldDisableDate={(date) =>
+                    date.getDay() !== 1 &&
+                    date.getDay() !== 2 &&
+                    date.getDay() !== 3 &&
+                    date.getDay() !== 4 &&
+                    date.getDay() !== 5
+                  }
+                />
+              </Box>}
     </>
   );
 }

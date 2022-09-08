a769a436966a0d8d573ece974091cd30684d6ab1
Luis <donlluque@gmail.com>
2022-09-07 22:02:17 -0300

    cambiossssss

--- api/src/routes/mercado_asociados.js
+++ api/src/routes/mercado_asociados.js
@@ -19,7 +19,12 @@ router.post("/", async (req, res, next) => {
       //unit_price: parseInt(req.body.price), TIENE QUE SER UN NUMERO por eso arriba esta el parseInt por si viene un STRING
     },
 
-    back_url: "https://www.google.com/", // redireccionar de nuevo a Wellness componente de PAGO EXITOSO
+    back_urls: {
+      success: "https://pf-wellness.vercel.app/", // redireccionar de nuevo a Wellness componente de PAGO EXITOSO
+      failure: "http://www.failure.com", //no la vamos a usar, por si falla el pago con tarjeta
+      pending: "http://www.pending.com", //no la vamos a usar, para pagos en efectivo o pedido de autorizacion de tarjeta
+    },
+    
     payer_email: "test_user_67403553@testuser.com",
   };
 
